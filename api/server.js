389c9cb0e370cc00e1caf79c284ac1f690c923b3
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-25 17:11:02 -0700

    added routes for login and register routes

--- api/server.js
+++ api/server.js
@@ -6,8 +6,18 @@ const server = express();
 
 configureMiddleware(server);
 
-server.get("/", (req, res) => {
-  res.status(200).json({ message: "This works!!!" });
-});
+
+
+// ---------- ROUTING ---------- //
+const registerRouter = require('../routes/register/register-router.js');
+const loginRouter = require('../routes/login/login-router.js');
+
+// Test Route
+// server.get("/", (req, res) => {
+//   res.status(200).json({ message: "This works!!!" });
+// });
+
+server.use('/api/register', registerRouter);
+server.use('/api/login', loginRouter);
 
 module.exports = server;
