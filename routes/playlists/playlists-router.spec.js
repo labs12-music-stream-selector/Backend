0cc7728232027f3e796f1568a23c398cead58af8
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-30 13:53:14 -0400

    added test for POST /

--- routes/playlists/playlists-router.spec.js
+++ routes/playlists/playlists-router.spec.js
@@ -1,21 +1,41 @@
 const request = require('supertest');
 const server = require('../../api/server.js');
 
+const url = '/api/user/playlists'; 
+
 describe('Playlists router', () => {
 	describe('GET user/playlists', () => {
 		it('should return status 200', async () => {
-			const res = await request(server).get('/api/user/playlists');
+			const res = await request(server).get(url);
 			expect(res.status).toBe(200);
 		});
 
 		it('should return array of playlists', (done) => {
-			return request(server).get('/api/user/playlists')
+			return request(server).get(url)
 				.expect([], done);
 		});
 
 		it('should return json', async () => {
-			const res = await request(server).get('/api/user/playlists');
+			const res = await request(server).get(url);
 			expect(res.type).toBe('application/json');
 		});
+	});
+
+	describe('POST /', () => {
+		it('should return status 201', async () => {
+			const res = await request(server).post(url).send({
+				userId: 1,
+				playlistName: 'Gaming Stream'
+			});
+			expect(res.status).toBe(201);
+		});
+
+		it('should return json', async () => {
+				const res = await request(server).post(url).send({
+				userId: 1,
+				playlistName: 'Gaming Stream'
+			});
+				expect(res.type).toBe('application/json');
+			});
 	});
 });
