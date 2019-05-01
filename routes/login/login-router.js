ea2008c6ca28aa8d222e5f30a652a93b9fdb091a
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-05-01 13:11:55 -0700

    pulled in data from master

--- routes/login/login-router.js
+++ routes/login/login-router.js
@@ -1,37 +1,51 @@
 const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const db = require('../../data/dbConfig.js');
 
-// router.get('/', (req, res) => {
-
-// })
-
-router.post('/', async (req, res) => {
+router.post('/oauth', async (req, res) => {
   try {
-    let { username, password } = req.body;
-    const user = await db('users').where({ username }).first();
-    if (user && bcrypt.compareSync(password, user.password)) {
-      const token = generateToken(user);
-      res.status(200).json({ token })
+    let { name, email, token } = req.body;
+    console.log(req.body);
+    const data = {
+      name,
+      email,
+      token
+    }
+    console.log("data: ", data);
+    if (user.token) {
+      console.log("first if passes")
+      const user = await db('users').where({ name: data.name }).first();
+      console.log("user: ", user)
+      if (user) {
+        console.log("second if passes")
+        res.status(200).json({ message: "Welcome back!", token })
+      } else {
+        console.log("second if fails")
+        const id = await db('users').insert(user).returning("id");
+        console.log("id: ", id)
+        if (id > 0) {
+          res.status(201).json({ message: "New user created", token })
+        } else {
+          res.status(500).json({ message: "internal server error" });
+        }
+      }
     } else {
-      res.status(500).json({ message: "Invalid Credentials" });
+      res.status(500).json({ message: "Error sending info to server" });
     }
   } catch (error) {
     res.status(500).json({ message: "internal server error" });
   }
 });
 
-function generateToken(user) {
-  const payload = {
-    subject: user.id,
-    username: user.username
-  }
-  secret = process.env.SECRET;
-  const options = {
-    expiresIn: '1d',
-  }
-  return jwt.sign(payload, secret, options);
-}
+// function generateToken(user) {
+//   const payload = {
+//     subject: user.id,
+//     username: user.username
+//   }
+//   secret = process.env.SECRET;
+//   const options = {
+//     expiresIn: '1d',
+//   }
+//   return jwt.sign(payload, secret, options);
+// }
 
 module.exports = router;
