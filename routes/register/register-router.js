133b84a3fecc45926c0efe6a896fcc81fbbfb22a
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-05-01 15:24:26 -0400

    Register Route created for OAuth

--- routes/register/register-router.js
+++ routes/register/register-router.js
@@ -1,26 +1,22 @@
 const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const db = require('../../data/dbConfig');
-
-router.post('/', async (req, res) => {
+      
+router.post('/oauth', async (req, res) => {
   try {
     let user = req.body;
-    if (!user.username || !user.password || !user.email) {
-      res.status(400).json({ message: "please fill in all fields" });
+    let {token, name, email} = user;
+    const existingUser =  await db('usersOAuth').where({email: email}).first();
+    if (existingUser) {
+      return res.status(201).json({ token, email, name }); 
     } else {
-      user.password = bcrypt.hashSync(user.password, 12);
-      const id = await db('users').insert(user).returning("id");
-      if (id > 0) {
-        res.status(201).json({ message: "Registration Complete", registered: true });
-      } else {
-        res.status(500).json({ message: "Pick a new username please" })
-      }
-
+      const id = await db('usersOAuth').insert(user).returning("id");
+      if (id) {
+        return res.status(200).json({ id, token, name, email });  
+      } 
     }
   } catch (error) {
     res.status(500).json({ message: "internal server error", error: error });
   }
 });
-
+      
 module.exports = router;
