1729a1c4f27c3a49d4afe1a7c77ce3c697f82937
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-05-02 14:53:10 -0400

    Users Route added, user can update and delete his profile

---
+++ routes/user/UserModel.js
@@ -0,0 +1,44 @@
+const db = require('../../data/dbConfig.js');
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById,
+  update,
+  deleteUser,
+};
+
+function update(id, user) {
+  return db('users')
+    .where('id', Number(id))
+    .update(user);
+}
+
+function deleteUser(id) {
+  return db('users')
+    .where({ id })
+    .delete()
+    
+}
+
+function find() {
+  return db('users').select('id', 'name', 'email');
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
+}
+
+async function add(user) {
+  const [id] = await db('users').insert(user)
+  .returning("id");
+  return findById(id);
+}
+
+function findById(id) {
+  return db('users')
+    .where({ id })
+    .select('id', 'name', 'email')
+    .first();
+}
