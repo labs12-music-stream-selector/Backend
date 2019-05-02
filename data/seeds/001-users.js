78efc758635012e636f132081630d3e86070d820
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-05-02 09:54:47 -0700

    added id column name for users table

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -8,52 +8,42 @@ exports.seed = function (knex, Promise) {
     {
       name: 'Kawsar',
       email: 'happygirl@yahoo.com',
-      patreon_support: false
     },
     {
       name: 'animeFan',
       email: 'animefan@yahoo.com',
-      patreon_support: false
     },
     {
       name: 'worldwideHandsome',
       email: 'handsome@yahoo.com',
-      patreon_support: false
     },
     {
       name: 'over9000',
       email: 'over9000@gmail.com',
-      patreon_support: false
     },
     {
       name: 'dancingMachine',
       email: 'dancingmachine@yahoo.com',
-      patreon_support: false
     },
     {
       name: 'bigvinaMac',
       email: 'bigvinamac@yahoo.com',
-      patreon_support: false
     },
     {
       name: 'TripleH',
       email: 'tripleh@gmail.com',
-      patreon_support: false
     },
     {
       name: 'hipHopFan',
       email: 'hiphopfan@gmail.com',
-      patreon_support: false
     },
     {
       name: 'Moonwalker',
       email: 'moonwalker@gmail.com',
-      patreon_support: false
     },
     {
       name: 'BTSStan',
       email: 'btsstan@gmail.com',
-      patreon_support: false
     }
   ])
 
