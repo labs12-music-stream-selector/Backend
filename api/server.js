a24e6d8c19c837779fec8aeb31c731805ea5218e
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-05-02 09:19:50 -0700

    added get song list route functionality

---
+++ api/server.js
@@ -0,0 +1,28 @@
+
+const express = require("express");
+
+const configureMiddleware = require("./middleware.js");
+
+const server = express();
+
+configureMiddleware(server);
+
+
+
+// ---------- ROUTING ---------- //
+const registerRouter = require('../routes/register/register-router.js');
+const loginRouter = require('../routes/login/login-router.js');
+const playlistsRouter = require('../routes/playlists/playlists-router.js');
+const songListRouter = require('../routes/song-list/song-list-router.js');
+
+// Test Route
+// server.get("/", (req, res) => {
+//   res.status(200).json({ message: "This works!!!" });
+// });
+
+server.use('/api/register', registerRouter);
+server.use('/api/login', loginRouter);
+server.use('/api/user/playlists', playlistsRouter);
+server.use('/api/song-list', songListRouter);
+
+module.exports = server;
