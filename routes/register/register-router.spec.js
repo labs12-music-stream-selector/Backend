a485618d71c7633f249b6512f1dba6f92e232f89
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-05-06 15:00:40 -0700

    fixed testing for current routes

--- routes/register/register-router.spec.js
+++ routes/register/register-router.spec.js
@@ -1,12 +1,9 @@
 const request = require('supertest');
 const server = require('../../api/server.js');
 const db = require('../../data/dbConfig');
-const bcrypt = require('bcryptjs');
 require("dotenv").config();
 
-const pass = bcrypt.hashSync('pass', 12);
-
-describe('OAuth login Route', () => {
+describe('OAuth register Route', () => {
   describe('GET /api/login/oauth', () => {
     beforeEach(async () => {
       await db('users').truncate();
@@ -15,15 +12,22 @@ describe('OAuth login Route', () => {
     it("should return a status 200", async () => {
       user = { name: "testregister", token: "liuahwefliuhawe.awerfihu34tihwef89.239rguweiufh9fnw349thf82398rfh", email: "testinger@tester.com" };
       const id = await db('users').insert(user);
-      const res = await request(server).post('/api/login/oauth').send(user);
+      const res = await request(server).post('/api/register/oauth').send(user);
       expect(res.status).toBe(200);
     })
 
     it('should return JSON', async () => {
       user = { name: "testregister", token: "liuahwefliuhawe.awerfihu34tihwef89.239rguweiufh9fnw349thf82398rfh", email: "testing@tester.com" };
       const id = await db('users').insert(user);
-      return request(server).post('/api/login/oauth').send(user).then(res => {
+      return request(server).post('/api/register/oauth').send(user).then(res => {
         expect(res.type).toBe('application/json')
+      })
+    })
+
+    it('should return a status 400 when no token is provided', async () => {
+      user = { name: "testregister", token: "liuahwefliuhawe.awerfihu34tihwef89.239rguweiufh9fnw349thf82398rfh", email: "testing@tester.com" };
+      return request(server).post('/api/register/oauth').send(user).then(res => {
+        expect(res.status).toBe(201);
       })
     })
   })
