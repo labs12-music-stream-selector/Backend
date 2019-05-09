077a6d934e9df67e64c21be2423a19e86e639b18
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-05-09 10:36:37 -0700

    removed all console logs

--- routes/register/register-router.js
+++ routes/register/register-router.js
@@ -5,24 +5,13 @@ router.post('/oauth', async (req, res) => {
   console.log("/oauth route");
   try {
     let user = req.body;
-    console.log("Line8 user: ", user);
     let { token, name, email } = user;
-    console.log("Line10 token: ", token);
-    console.log("Line11 name: ", name);
-    console.log("Line12 email: ", email);
-    //console.log(user)
     const existingUser = await db('users').where({ 'email': email }).first();
-    console.log("Line15 existingUser: ", existingUser);
-    //console.log(existingUser)
     if (existingUser) {
-      console.log("if existingUser true");
       return res.status(200).json({ id: existingUser.id, token, email, name });
     } else {
-      console.log("if existingUser false");
       const [id] = await db('users').insert(user).returning("id");
-      console.log("id: ", id);
       if (id) {
-        console.log("id is true");
         return res.status(201).json({ id, token, name, email });
       }
     }
