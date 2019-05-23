f50c7e19ed40d5e6e5bfdfb6a13bb1bbd6371b49
Kawsar Hussen <kawsarhussen@gmail.com>
2019-05-23 16:22:35 -0400

    new token will be updated and then user will be getting accesss

--- routes/register/register-router.js
+++ routes/register/register-router.js
@@ -8,8 +8,10 @@ router.post('/oauth', async (req, res) => {
     let { token, name, email } = user;
     const existingUser = await db('users').where({ 'email': email }).select('id').first();
     if (existingUser) {
-      console.log(existingUser);
-      return res.status(200).json({ id: existingUser.id, token, email, name });
+      const red = await db('users').where("id", existingUser.id).update(user).returning("id");
+      if(red){
+        return res.status(200).json({ id: existingUser.id, token, email, name });
+      }
     } else {
       const [id] = await db('users').insert(user).returning("id");
       if (id) {
